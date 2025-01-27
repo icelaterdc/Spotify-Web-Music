8d70774599621f64491567eea2a8f7948e5039b9
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-01-27 04:15:38 +0300

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -248,7 +248,7 @@ function Dashboard() {
               <h3 className="text-2xl font-bold mb-4 flex items-center gap-2">
                 <Library className="text-lyra-highlight" />
                 Çalma Listelerin
-                  <h3>
+              </h3>
               <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                 {playlists.map(playlist => (
                   <div
